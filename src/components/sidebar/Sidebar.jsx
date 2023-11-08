b25b23f7d0524bde11542a7211040f90dfe97fb1
Diana <dayancrgen@gmail.com>
2023-11-07 18:08:50 -0600

    init

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,22 @@
+import './sidebar.scss'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+        <div className="top">
+          <span className="logo">diana</span>
+        </div>
+        <div className="center">
+          <ul>
+            <li><span>Dashboard</span></li>
+            <li><span>Dashboard</span></li>
+            <li><span>Dashboard</span></li>
+            <li><span>Dashboard</span></li>
+          </ul>
+        </div>
+        <div className="bottom">color options</div>
+    </div>
+  )
+}
+
+export default Sidebar

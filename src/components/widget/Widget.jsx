d97d77eec916f0918646eed8687cd5418578008b
Diana <dayancrgen@gmail.com>
2023-11-10 16:24:17 -0600

    widget

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,14 +1,60 @@
 import './widget.scss'
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import AccountBalanceOutlinedIcon from '@mui/icons-material/AccountBalanceOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import MonetizationOnOutlinedIcon from '@mui/icons-material/MonetizationOnOutlined';
 
 const Widget = ({ type }) => {
+  let data;
+    //temporary
+    const amount = 200
+    const diff = 20
+
+    switch(type){
+          case "user":
+            data = {
+              title: "USERS",
+              isMoney: false, 
+              link: "See all users",
+              icon: <PersonOutlinedIcon className='icon'/>,
+            };
+            break;
+            case "order":
+              data = {
+                title: "ORDERS",
+                isMoney: false, 
+                link: "View all orders",
+                icon: <ShoppingCartOutlinedIcon className='icon'/>,
+              };
+              break;
+              case "earning":
+                data = {
+                  title: "ERARNINGS",
+                  isMoney: true, 
+                  link: "View net earnings",
+                  icon: <MonetizationOnOutlinedIcon className='icon'/>,
+                };
+                break;
+                case "balance":
+                  data = {
+                    title: "BALANCE",
+                    isMoney: true, 
+                    link: "See details",
+                    icon: <AccountBalanceOutlinedIcon className='icon'/>,
+                  };
+                  break;
+            default:
+              break;
+    }
+
+
   return (
     <div className="widget">
       <div className="left">
-        <span className="title">USERS</span>
-        <span className="counter">231231</span>
-        <span className="link">See all user</span>
+        <span className="title">{data.title}</span>
+        <span className="counter">{data.isMoney && "$"}{amount}</span>
+        <span className="link">{data.link}</span>
       </div>
       <div className="right">
         <div className="percentage positive">
